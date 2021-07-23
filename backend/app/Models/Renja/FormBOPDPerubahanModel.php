10e26377dad1bbf6b7d15bfa55ad61abfdad3eaa
Mochammad Rizki Romdoni <mr.romdoni@gmail.com>
2021-07-23 16:33:37 +0700

    perbaiki laporan form b opd

--- backend/app/Models/Renja/FormBOPDPerubahanModel.php
+++ backend/app/Models/Renja/FormBOPDPerubahanModel.php
@@ -157,8 +157,8 @@ class FormBOPDPerubahanModel extends ReportModel
         $totalPaguOPD = (float)\DB::table('trRKA')
                                     ->where('OrgID',$OrgID)                                            
                                     ->where('TA',$tahun)  
-                                    ->where('EntryLvl',1)
-                                    ->sum('PaguDana1');        
+                                    ->where('EntryLvl',2)
+                                    ->sum('PaguDana2');        
         
         
         $no_huruf=ord('A');
@@ -223,11 +223,11 @@ class FormBOPDPerubahanModel extends ReportModel
                     $sheet->setCellValue("C$row",$data_kegiatan->Nm_Kegiatan);  
 
                     $daftar_sub_kegiatan = \DB::table('trRKA')
-                                    ->select(\DB::raw('`RKAID`,`kode_sub_kegiatan`,`Nm_Sub_Kegiatan`,`PaguDana1`,`lokasi_kegiatan1`, `Nm_Sub_Organisasi`'))
+                                    ->select(\DB::raw('`RKAID`,`kode_sub_kegiatan`,`Nm_Sub_Kegiatan`,`PaguDana2`,`lokasi_kegiatan2`, `Nm_Sub_Organisasi`'))
                                     ->where('kode_kegiatan',$kode_kegiatan)
                                     ->where('OrgID',$OrgID)
                                     ->where('TA',$tahun)
-                                    ->where('EntryLvl',1)
+                                    ->where('EntryLvl',2)
                                     ->orderBy('kode_sub_kegiatan','ASC')
                                     ->get();
                     
@@ -236,8 +236,8 @@ class FormBOPDPerubahanModel extends ReportModel
                         $pagu_dana_kegiatan = (float)\DB::table('trRKA')
                                     ->where('OrgID',$OrgID)
                                     ->where('kode_kegiatan',$kode_kegiatan)
-                                    ->where('EntryLvl',1)
-                                    ->sum('PaguDana1');
+                                    ->where('EntryLvl',2)
+                                    ->sum('PaguDana2');
 
                         $jumlah_uraian_kegiatan = 0;
 
@@ -253,13 +253,13 @@ class FormBOPDPerubahanModel extends ReportModel
                         $no_sub_kegiatan = 1;
                         foreach ($daftar_sub_kegiatan as $data_sub_kegiatan)
                         {
-                            $pagu_dana_program += $data_sub_kegiatan->PaguDana1;
-                            $pagu_dana_kegiatan += $data_sub_kegiatan->PaguDana1;
+                            $pagu_dana_program += $data_sub_kegiatan->PaguDana2;
+                            $pagu_dana_kegiatan += $data_sub_kegiatan->PaguDana2;
 
                             $RKAID=$data_sub_kegiatan->RKAID;
                             $kode_sub_kegiatan = $data_sub_kegiatan->kode_sub_kegiatan;
 
-                            $persen_bobot=Helper::formatPersen($data_sub_kegiatan->PaguDana1,$totalPaguOPD);
+                            $persen_bobot=Helper::formatPersen($data_sub_kegiatan->PaguDana2,$totalPaguOPD);
                             $totalPersenBobot+=$persen_bobot;
 
                             //jumlah baris uraian
@@ -268,15 +268,15 @@ class FormBOPDPerubahanModel extends ReportModel
                             $jumlah_uraian_kegiatan += $jumlahuraian;
 
                             $data_target=\DB::table('trRKATargetRinc')
-                                            ->select(\DB::raw('COALESCE(SUM(target1),0) AS totaltarget, COALESCE(SUM(fisik1),0) AS jumlah_fisik'))
+                                            ->select(\DB::raw('COALESCE(SUM(target2),0) AS totaltarget, COALESCE(SUM(fisik2),0) AS jumlah_fisik'))
                                             ->where('RKAID',$RKAID)
-                                            ->where('bulan1','<=',$no_bulan)
+                                            ->where('bulan2','<=',$no_bulan)
                                             ->get();
 
                             $data_realisasi=\DB::table('trRKARealisasiRinc')
-                                        ->select(\DB::raw('COALESCE(SUM(realisasi1),0) AS realisasi1, COALESCE(SUM(fisik1),0) AS fisik1'))
+                                        ->select(\DB::raw('COALESCE(SUM(realisasi2),0) AS realisasi2, COALESCE(SUM(fisik2),0) AS fisik2'))
                                         ->where('RKAID',$RKAID)
-                                        ->where('bulan1','<=',$no_bulan)
+                                        ->where('bulan2','<=',$no_bulan)
                                         ->get();
 
                             //menghitung persen target fisik
@@ -287,9 +287,9 @@ class FormBOPDPerubahanModel extends ReportModel
                             $totalPersenTargetFisik+=$persen_target_fisik;
 
                             //menghitung persen realisasi fisik
-                            $realisasi_fisik_program += $data_realisasi[0]->fisik1;
-                            $realisasi_fisik_kegiatan += $data_realisasi[0]->fisik1;
-                            $persen_realisasi_fisik=Helper::formatPecahan($data_realisasi[0]->fisik1,$jumlahuraian);
+                            $realisasi_fisik_program += $data_realisasi[0]->fisik2;
+                            $realisasi_fisik_kegiatan += $data_realisasi[0]->fisik2;
+                            $persen_realisasi_fisik=Helper::formatPecahan($data_realisasi[0]->fisik2,$jumlahuraian);
                             $totalPersenRealisasiFisik+=$persen_realisasi_fisik;
 
                             $persen_tertimbang_fisik=0.00;
@@ -304,13 +304,13 @@ class FormBOPDPerubahanModel extends ReportModel
                             $target_keuangan_program += $totalTargetKeuangan;
                             $target_keuangan_kegiatan += $totalTargetKeuangan;
                             $totalTargetKeuanganKeseluruhan+=$totalTargetKeuangan;
-                            $persen_target_keuangan=Helper::formatPersen($totalTargetKeuangan,$data_sub_kegiatan->PaguDana1);
+                            $persen_target_keuangan=Helper::formatPersen($totalTargetKeuangan,$data_sub_kegiatan->PaguDana2);
 
-                            $totalRealisasiKeuangan=$data_realisasi[0]->realisasi1;
+                            $totalRealisasiKeuangan=$data_realisasi[0]->realisasi2;
                             $realisasi_keuangan_program += $totalRealisasiKeuangan;
                             $realisasi_keuangan_kegiatan += $totalRealisasiKeuangan;
                             $totalRealisasiKeuanganKeseluruhan+=$totalRealisasiKeuangan;
-                            $persen_realisasi_keuangan=Helper::formatPersen($totalRealisasiKeuangan,$data_sub_kegiatan->PaguDana1);
+                            $persen_realisasi_keuangan=Helper::formatPersen($totalRealisasiKeuangan,$data_sub_kegiatan->PaguDana2);
 
                             $persen_tertimbang_keuangan=0.00;
                             if ($persen_realisasi_keuangan > 0 && $persen_bobot > 0)
@@ -319,18 +319,18 @@ class FormBOPDPerubahanModel extends ReportModel
                             }
                             $total_ttb_keuangan += $persen_tertimbang_keuangan;
 
-                            $sisa_anggaran=$data_sub_kegiatan->PaguDana1-$totalRealisasiKeuangan;
+                            $sisa_anggaran=$data_sub_kegiatan->PaguDana2-$totalRealisasiKeuangan;
                             $totalSisaAnggaran+=$sisa_anggaran;
 
-                            $persen_sisa_anggaran=Helper::formatPersen($sisa_anggaran,$data_sub_kegiatan->PaguDana1);
+                            $persen_sisa_anggaran=Helper::formatPersen($sisa_anggaran,$data_sub_kegiatan->PaguDana2);
                             
                             $sheet->setCellValue("A$row",chr($no_huruf) .'.'.$no_kegiatan.'.'.$no_sub_kegiatan);
                             $sheet->setCellValue("B$row",$kode_sub_kegiatan);  
                             $sheet->setCellValue("C$row",$data_sub_kegiatan->Nm_Sub_Kegiatan);  
                             $sheet->setCellValue("D$row",$data_sub_kegiatan->Nm_Sub_Organisasi);  
-                            $sheet->setCellValue("E$row",Helper::formatUang($data_sub_kegiatan->PaguDana1));  
+                            $sheet->setCellValue("E$row",Helper::formatUang($data_sub_kegiatan->PaguDana2));  
                             $sheet->setCellValue("F$row",$persen_bobot);  
-                            $sheet->setCellValue("GF$row",$persen_target_fisik);  
+                            $sheet->setCellValue("G$row",$persen_target_fisik);  
                             $sheet->setCellValue("H$row",$persen_realisasi_fisik);  
                             $sheet->setCellValue("I$row",$persen_tertimbang_fisik);  
                             $sheet->setCellValue("J$row",Helper::formatUang($totalTargetKeuangan));  
@@ -338,7 +338,7 @@ class FormBOPDPerubahanModel extends ReportModel
                             $sheet->setCellValue("L$row",Helper::formatUang($totalRealisasiKeuangan));  
                             $sheet->setCellValue("M$row",$persen_realisasi_keuangan);  
                             $sheet->setCellValue("N$row",$persen_tertimbang_keuangan);  
-                            $sheet->setCellValue("O$row",$data_sub_kegiatan->lokasi_kegiatan1);  
+                            $sheet->setCellValue("O$row",$data_sub_kegiatan->lokasi_kegiatan2);  
                             $sheet->setCellValue("P$row",Helper::formatUang($sisa_anggaran));  
                             $sheet->setCellValue("Q$row",$persen_sisa_anggaran);
                             $row += 1; 
@@ -355,6 +355,7 @@ class FormBOPDPerubahanModel extends ReportModel
                             $persen_tertimbang_fisik=number_format(($persen_realisasi_fisik*$persen_bobot)/100,2);
                         }
                         
+                        $persen_target_keuangan=Helper::formatPersen($target_keuangan_kegiatan,$pagu_dana_kegiatan);
                         $persen_realisasi_keuangan=Helper::formatPersen($realisasi_keuangan_kegiatan,$pagu_dana_kegiatan);
                         $persen_tertimbang_keuangan=0.00;
                         if ($persen_realisasi_keuangan > 0 && $persen_bobot > 0)
