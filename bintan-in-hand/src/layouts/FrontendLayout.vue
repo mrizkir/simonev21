f48984f02419eb47a3d67dd03d6aa0c8430de6ed
Mochammad Rizki Romdoni <mr.romdoni@gmail.com>
2023-03-08 15:26:40 +0700

    front layout

--- bintan-in-hand/src/layouts/FrontendLayout.vue
+++ bintan-in-hand/src/layouts/FrontendLayout.vue
@@ -1,5 +1,9 @@
 <template>
-  <div>
-    test
-  </div>
+  <v-layout>
+    <v-app-bar title="Bintan in Hand"></v-app-bar>
+    <v-navigation-drawer color="red" permanent></v-navigation-drawer>
+    <v-main>
+      <v-slot />
+    </v-main>
+  </v-layout>
 </template>
