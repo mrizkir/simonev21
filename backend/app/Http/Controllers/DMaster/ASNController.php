82d3d97f4a1f2e5d44800f154abee36e4f2b55e0
Mochammad Rizki Romdoni <mr.romdoni@gmail.com>
2023-06-22 05:53:36 +0700

    formatted currency

--- backend/app/Http/Controllers/DMaster/ASNController.php
+++ backend/app/Http/Controllers/DMaster/ASNController.php
@@ -11,184 +11,184 @@ use Ramsey\Uuid\Uuid;
 
 class ASNController extends Controller
 {    
-	/**
-	 * mendapatkan daftar seluruh ASN
-	 *
-	 * @return \Illuminate\Http\Response
-	 */
-	public function index(Request $request)
-	{
-		$this->hasPermissionTo('DMASTER-ASN_BROWSE');
-		
-		$this->validate($request, [            
-			'tahun'=>'required',            
-		]);     
-		
-		$tahun=$request->input('tahun');
-		
-		$data = ASNModel::where('TA',$tahun)
-			->orderBy('Nm_ASN', 'ASC')
-			->get();
-		
-		return Response()->json([
-			'status'=>1,
-			'pid'=>'fetchdata',
-			'asn'=>$data,
-			'message'=>'Fetch data ASN berhasil diperoleh'
-		], 200);  
+  /**
+   * mendapatkan daftar seluruh ASN
+   *
+   * @return \Illuminate\Http\Response
+   */
+  public function index(Request $request)
+  {
+    $this->hasPermissionTo('DMASTER-ASN_BROWSE');
+    
+    $this->validate($request, [            
+      'tahun'=>'required',            
+    ]);     
+    
+    $tahun=$request->input('tahun');
+    
+    $data = ASNModel::where('TA',$tahun)
+      ->orderBy('Nm_ASN', 'ASC')
+      ->get();
+    
+    return Response()->json([
+      'status'=>1,
+      'pid'=>'fetchdata',
+      'asn'=>$data,
+      'message'=>'Fetch data ASN berhasil diperoleh'
+    ], 200);  
 
-	}
-	/**
-	 * Store a newly created resource in storage.
-	 *
-	 * @param  \Illuminate\Http\Request  $request
-	 * @return \Illuminate\Http\Response
-	 */
-	public function store(Request $request)
-	{        
-		$this->hasPermissionTo('DMASTER-ASN_STORE');
-		$this->validate($request, [           
-			'TA'=>'required'
-		]);
+  }
+  /**
+   * Store a newly created resource in storage.
+   *
+   * @param  \Illuminate\Http\Request  $request
+   * @return \Illuminate\Http\Response
+   */
+  public function store(Request $request)
+  {        
+    $this->hasPermissionTo('DMASTER-ASN_STORE');
+    $this->validate($request, [           
+      'TA'=>'required'
+    ]);
 
-		$ta = $request->input('TA');
+    $ta = $request->input('TA');
 
-		$this->validate($request, [
-			'NIP_ASN'=> [
-				Rule::unique('tmASN')->where(function($query) use ($request) {
-					return $query->where('NIP_ASN',$request->input('NIP_ASN'))
-						->where('TA',$request->input('TA'));
-				}),
-				'required',
-				'regex:/^[0-9]+$/'
-			],
-			'Nm_ASN'=>'required',
-		]);
-			  
-		$asn = ASNModel::create ([
-			'ASNID'=> Uuid::uuid4()->toString(),
-			'NIP_ASN' => $request->input('NIP_ASN'),
-			'Nm_ASN' => $request->input('Nm_ASN'),
-			'Descr' => $request->input('Descr'),
-			'TA'=>$ta,
-		]);  
-		
-		return Response()->json([
-			'status'=>1,
-			'pid'=>'store',
-			'asn'=>$asn,                                    
-			'message'=>'Data ASN berhasil disimpan.'
-		], 200); 		
-	}
-	/**
-	 * menyalin asli
-	 *
-	 * @param  \Illuminate\Http\Request  $request
-	 * @return \Illuminate\Http\Response
-	 */
-	public function salin(Request $request)
-	{
-		$this->validate($request, [            
-			'tahun_asal'=>'required|numeric',
-			'tahun_tujuan'=>'required|numeric|gt:tahun_asal',
-		]);
+    $this->validate($request, [
+      'NIP_ASN'=> [
+        Rule::unique('tmASN')->where(function($query) use ($request) {
+          return $query->where('NIP_ASN',$request->input('NIP_ASN'))
+            ->where('TA',$request->input('TA'));
+        }),
+        'required',
+        'regex:/^[0-9]+$/'
+      ],
+      'Nm_ASN'=>'required',
+    ]);
+        
+    $asn = ASNModel::create ([
+      'ASNID'=> Uuid::uuid4()->toString(),
+      'NIP_ASN' => $request->input('NIP_ASN'),
+      'Nm_ASN' => $request->input('Nm_ASN'),
+      'Descr' => $request->input('Descr'),
+      'TA'=>$ta,
+    ]);  
+    
+    return Response()->json([
+      'status'=>1,
+      'pid'=>'store',
+      'asn'=>$asn,                                    
+      'message'=>'Data ASN berhasil disimpan.'
+    ], 200); 		
+  }
+  /**
+   * menyalin asli
+   *
+   * @param  \Illuminate\Http\Request  $request
+   * @return \Illuminate\Http\Response
+   */
+  public function salin(Request $request)
+  {
+    $this->validate($request, [            
+      'tahun_asal'=>'required|numeric',
+      'tahun_tujuan'=>'required|numeric|gt:tahun_asal',
+    ]);
 
-		$tahun_asal = $request->input('tahun_asal');
-		$tahun_tujuan = $request->input('tahun_tujuan');
+    $tahun_asal = $request->input('tahun_asal');
+    $tahun_tujuan = $request->input('tahun_tujuan');
 
-		$str_insert = '
-			INSERT INTO `tmASN` (
-				`ASNID`,
-				`NIP_ASN`,
-				`Nm_ASN`,
-				`Descr`,
-				`TA`,
-				`Active`,        
+    $str_insert = '
+      INSERT INTO `tmASN` (
+        `ASNID`,
+        `NIP_ASN`,
+        `Nm_ASN`,
+        `Descr`,
+        `TA`,
+        `Active`,        
         created_at,
         updated_at
-			)		
-			SELECT
-				uuid() AS id,
-				
+      )		
+      SELECT
+        uuid() AS id,
+        
         `NIP_ASN`,
-				`Nm_ASN`,
-				"DI IMPOR DARI TAHUN '.$tahun_asal.'" AS `Descr`,
-				'.$tahun_tujuan.' AS `TA`,
-				`Active`,                
-				NOW() AS created_at,
-				NOW() AS updated_at
-			FROM tmASN t1
-			WHERE `TA`='.$tahun_asal.'
-			AND `NIP_ASN` NOT IN (SELECT `NIP_ASN` FROM `tmASN` WHERE `TA`='.$tahun_tujuan.')      			
-		';    
+        `Nm_ASN`,
+        "DI IMPOR DARI TAHUN '.$tahun_asal.'" AS `Descr`,
+        '.$tahun_tujuan.' AS `TA`,
+        `Active`,                
+        NOW() AS created_at,
+        NOW() AS updated_at
+      FROM tmASN t1
+      WHERE `TA`='.$tahun_asal.'
+      AND `NIP_ASN` NOT IN (SELECT `NIP_ASN` FROM `tmASN` WHERE `TA`='.$tahun_tujuan.')      			
+    ';    
     
-		\DB::statement($str_insert); 
+    \DB::statement($str_insert); 
     
-		return Response()->json([
-			'status'=>1,
-			'pid'=>'store',            
-			'message'=>"Salin ASN dari tahun anggaran $tahun_asal berhasil.",
-			'sql_insert'=>$str_insert,
-		], 200);
-	}
-	/**
-	 * Store a newly created resource in storage.
-	 *
-	 * @param  \Illuminate\Http\Request  $request
-	 * @return \Illuminate\Http\Response
-	 */
-	public function update(Request $request,$id)
-	{        
-		$this->hasPermissionTo('DMASTER-ASN_UPDATE');
-		$asn = ASNModel::find($id);
-		
-		$this->validate($request, [    
-			'NIP_ASN'=>[
-				Rule::unique('tmASN')->where(function($query) use ($request,$asn) {  
-					if ($request->input('NIP_ASN')==$asn->NIP_ASN) 
-					{
-						return $query->where('NIP_ASN','ignore');
-					}                 
-					else
-					{
-						return $query->where('NIP_ASN',$request->input('NIP_ASN'))
-								->where('TA',$asn->TA);
-					}                                                                                    
-				}),
-				'required',
-				'regex:/^[0-9]+$/'
-			],
-			'Nm_ASN'=>'required',
-		]);		
-		
-		$asn->NIP_ASN = $request->input('NIP_ASN');
-		$asn->Nm_ASN = $request->input('Nm_ASN');
-		$asn->Descr = $request->input('Descr');
-		$asn->save();
+    return Response()->json([
+      'status'=>1,
+      'pid'=>'store',            
+      'message'=>"Salin ASN dari tahun anggaran $tahun_asal berhasil.",
+      'sql_insert'=>$str_insert,
+    ], 200);
+  }
+  /**
+   * Store a newly created resource in storage.
+   *
+   * @param  \Illuminate\Http\Request  $request
+   * @return \Illuminate\Http\Response
+   */
+  public function update(Request $request,$id)
+  {        
+    $this->hasPermissionTo('DMASTER-ASN_UPDATE');
+    $asn = ASNModel::find($id);
+    
+    $this->validate($request, [    
+      'NIP_ASN'=>[
+        Rule::unique('tmASN')->where(function($query) use ($request,$asn) {  
+          if ($request->input('NIP_ASN')==$asn->NIP_ASN) 
+          {
+            return $query->where('NIP_ASN','ignore');
+          }                 
+          else
+          {
+            return $query->where('NIP_ASN',$request->input('NIP_ASN'))
+                ->where('TA',$asn->TA);
+          }                                                                                    
+        }),
+        'required',
+        'regex:/^[0-9]+$/'
+      ],
+      'Nm_ASN'=>'required',
+    ]);		
+    
+    $asn->NIP_ASN = $request->input('NIP_ASN');
+    $asn->Nm_ASN = $request->input('Nm_ASN');
+    $asn->Descr = $request->input('Descr');
+    $asn->save();
 
-		return Response()->json([
-			'status'=>1,
-			'pid'=>'update',
-			'asn'=>$asn,                                    
-			'message'=>'Data ASN '.$asn->Nm_ASN.' berhasil diubah.'
-		], 200);
-		
-	}
-	/**
-	 * Remove the specified resource from storage.
-	 *
-	 * @param  int  $uuid
-	 * @return \Illuminate\Http\Response
-	 */
-	public function destroy(Request $request,$id)
-	{   
-		$this->hasPermissionTo('DMASTER-ASN_DESTROY');
-		$asn = ASNModel::find($id);
-		$result=$asn->delete();
-		return Response()->json([
-			'status'=>1,
-			'pid'=>'destroy',                
-			'message'=>"Data ASN dengan ID ($id) berhasil dihapus"
-		], 200);
-	}
+    return Response()->json([
+      'status'=>1,
+      'pid'=>'update',
+      'asn'=>$asn,                                    
+      'message'=>'Data ASN '.$asn->Nm_ASN.' berhasil diubah.'
+    ], 200);
+    
+  }
+  /**
+   * Remove the specified resource from storage.
+   *
+   * @param  int  $uuid
+   * @return \Illuminate\Http\Response
+   */
+  public function destroy(Request $request,$id)
+  {   
+    $this->hasPermissionTo('DMASTER-ASN_DESTROY');
+    $asn = ASNModel::find($id);
+    $result=$asn->delete();
+    return Response()->json([
+      'status'=>1,
+      'pid'=>'destroy',                
+      'message'=>"Data ASN dengan ID ($id) berhasil dihapus"
+    ], 200);
+  }
 }
